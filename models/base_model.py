6d9ae14e6e1acd0fbdd6f217ed1497f0afa05a23
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-03-11 10:25:46 +0200

    3. BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Base Model class"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize BaseModel attributes"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                else:
+                    setattr(self, "_BaseModel__class", value)
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f'))
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return string representation of BaseModel"""
+        return "[{}] ({}) {}".format(type(self).__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """Update updated_at with current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return dictionary representation of BaseModel"""
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = type(self).__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy
