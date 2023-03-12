a9157a3d33b8af1135f3ac9655e7272616b22500
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-03-12 16:38:33 +0200

    Try this

--- models/user.py
+++ models/user.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 """Class User module"""
 from models.base_model import BaseModel
+import datetime
+import uuid
 
 
 class User(BaseModel):
-    """Class User module"""
-
+    """
+    class 'User' that inherits from parent class - BaseModel
+    """
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of User.
+        """
+        super().__init__(*args, **kwargs)
